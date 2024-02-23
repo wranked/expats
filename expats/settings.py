38c19aacd749efa90b506a209b31d3a0294cf4cd
Gustavo Tempel <gtempel@hotmail.com>
2024-02-23 12:23:17 +0100

    Modified config for Vercel

--- expats/settings.py
+++ expats/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+lo1i#g@a8tt(+at@a*&ji-bxmro+og9ne^n4b%3e3!+de@ueg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -131,8 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
