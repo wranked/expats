ec010e77d531ba3d85e7c5a8bddf48b617fefcda
Gustavo Tempel <gtempel@hotmail.com>
2024-02-23 12:34:42 +0100

    Modified config for Vercel

--- expats/settings.py
+++ expats/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
+//STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "staticfiles")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
